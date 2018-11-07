0a044740638c44c35e6de37a9cd4b818cda483f1
Kaj Ström <kaj.strom@kapsi.fi>
2018-11-07 21:40:03 +0200

    Day 1, part two

--- src/advent_of_code_2017/day1.clj
+++ src/advent_of_code_2017/day1.clj
@@ -6,7 +6,17 @@
 (defn matches-or-zero [[x y]]
   (if (= x y) x 0))
 
+(defn str->int-vector [str]
+  (map parse-int (clojure.string/split str #"")))
+
 (defn captcha [digits]
-  (let [int-digits (map parse-int (clojure.string/split digits #""))
+  (let [int-digits (str->int-vector digits)
         pairs (partition 2 1 int-digits int-digits)]
      (apply + (map matches-or-zero pairs))))
+
+(defn captcha2 [digits]
+  (let [int-digits (str->int-vector digits)
+        step (/ (count int-digits) 2)
+        digits-cont (concat int-digits (take step int-digits))
+        pairs (map-indexed (fn [idx digit] [digit (nth digits-cont (+ idx step))]) int-digits)]
+    (apply + (map matches-or-zero pairs))))
--- test/advent_of_code_2017/day1_test.clj
+++ test/advent_of_code_2017/day1_test.clj
@@ -1,6 +1,6 @@
 (ns advent-of-code-2017.day1-test
   (:require [clojure.test :refer :all]
-            [advent-of-code-2017.day1 :refer [captcha]]))
+            [advent-of-code-2017.day1 :refer [captcha captcha2]]))
 
 (deftest captcha-test
   (testing "captcha calculation"
@@ -8,3 +8,11 @@
     (is (= 3 (captcha "1122")))
     (is (= 0 (captcha "1234")))
     (is (= 9 (captcha "91212129")))))
+
+(deftest captcha-part2-test
+  (testing "captcha calculation"
+    (is (= 6 (captcha2 "1212")))
+    (is (= 0 (captcha2 "1221")))
+    (is (= 4 (captcha2 "123425")))
+    (is (= 12 (captcha2 "123123")))
+    (is (= 4 (captcha2 "12131415")))))
