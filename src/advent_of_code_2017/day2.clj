bb4d81fd8f88cec4ba1c7faa0e7df2ee12213d53
Kaj Ström <kaj.strom@kapsi.fi>
2018-11-08 22:14:53 +0200

    Day 2, part two

--- src/advent_of_code_2017/day2.clj
+++ src/advent_of_code_2017/day2.clj
@@ -7,15 +7,37 @@
        (map #(clojure.string/split % #"\t"))
        (map #(map parse-int %))))
 
-(defn diff-of-smallest-and-largest [row]
+(defn diff-of-min-max [row]
   (- (apply max row) (apply min row)))
 
-(defn checksum [str]
+(defn evenly-divisible
+  "Returns division if x & y can be evenly divided and are not the same number.
+  Otherwise returns nil."
+  [x y]
+  (let [smaller (min x y)
+        larger (max x y)]
+    (if-not (= smaller larger)
+      (if (= 0 (rem larger smaller))
+        (/ larger smaller)
+        nil)
+      nil)))
+
+(defn even-division [row]
+  (->> (map (fn [digit] (some #(evenly-divisible % digit) row)) row)
+       (filter some?)
+       first))
+
+(defn checksum [calc-fn str]
   (->> (str->int-vectors str)
-       (map diff-of-smallest-and-largest)
+       (map calc-fn)
        (apply +)))
 
 (defn checksum-of-file [file]
-  (-> (resource file)
-      slurp
-      checksum))
+  (->> (resource file)
+       slurp
+       (checksum diff-of-min-max)))
+
+(defn checksum-of-file2 [file]
+  (->> (resource file)
+       slurp
+       (checksum even-division)))
--- test/advent_of_code_2017/day2_test.clj
+++ test/advent_of_code_2017/day2_test.clj
@@ -1,7 +1,11 @@
 (ns advent-of-code-2017.day2-test
   (:require [clojure.test :refer :all]
-            [advent-of-code-2017.day2 :refer [checksum-of-file]]))
+            [advent-of-code-2017.day2 :refer [checksum-of-file checksum-of-file2]]))
 
 (deftest checksum-1-test
   (testing "checksum calculation"
     (is (= 36766 (checksum-of-file "day2-1.txt")))))
+
+(deftest checksum-2-test
+  (testing "checksum calculation"
+    (is (= 261 (checksum-of-file2 "day2-1.txt")))))
