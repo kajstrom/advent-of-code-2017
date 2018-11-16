233de67520bd4d6a67178347c0aac9e66a952f22
Kaj Ström <kaj.strom@kapsi.fi>
2018-11-16 23:13:15 +0200

    Day 5, part two

--- src/advent_of_code_2017/day5.clj
+++ src/advent_of_code_2017/day5.clj
@@ -7,7 +7,12 @@
 (defn idx-inside-list [idx list]
   (and (>= idx 0) (< idx (count list))))
 
-(defn travel-jumps [list]
+(defn modify-jump [jump]
+  (if (>= jump 3)
+    (dec jump)
+    (inc jump)))
+
+(defn travel-jumps [jump-fn list]
   (let [list (atom list)
         idx (atom 0)
         steps (atom 0)]
@@ -15,14 +20,22 @@
       (do
         (swap! steps inc)
         (let [next-idx (+ @idx (get @list @idx))]
-          (reset! list (update @list @idx inc))
+          (reset! list (update @list @idx jump-fn))
           (reset! idx next-idx))))
     @steps))
 
-(defn travel-steps-from-file [file]
+(defn travel-steps-from-file1 [file]
+  (->> (resource file)
+       slurp
+       (clojure.string/split-lines)
+       (map parse-int)
+       vec
+       (travel-jumps inc)))
+
+(defn travel-steps-from-file2 [file]
   (->> (resource file)
       slurp
       (clojure.string/split-lines)
        (map parse-int)
        vec
-       travel-jumps))
+       (travel-jumps modify-jump)))
